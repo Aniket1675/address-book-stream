a4552833d07d3e6d4d77ef4d880312c52e285cc3
Aniket1675 <Aniket@05>
2022-04-06 23:11:23 +0530

    ADDED: feature to edit exsting contact in address book.

--- src/com/bridgelabz/addressbook/AddressBookSystemDetails.java
+++ src/com/bridgelabz/addressbook/AddressBookSystemDetails.java
@@ -45,6 +45,60 @@ public class AddressBookSystemDetails {
     }
 
     /*
+    This method is used to edit the details in address book
+     */
+    public void editDetails() {
+        System.out.println("Confirm your first name to edit details: ");
+        String confirmName = sc.next();
+
+        for (int i = 0; i < personDetails.size(); i++) {
+            if (personDetails.get(i).getFirstName().equals(confirmName)) {
+                System.out.println("Select mentioned detail to edit: ");
+                System.out.println("\n1.First Name\n2.Last Name\n3.Address\n4.city\n5.State\n6.Zip\n7.Mobile number\n8.Email");
+                int edit = sc.nextInt();
+
+                switch (edit) {
+                    case 1:
+                        System.out.println("Enter first name: ");
+                        personDetails.get(i).setFirstName(sc.next());
+                        break;
+                    case 2:
+                        System.out.println("Enter Last name: ");
+                        personDetails.get(i).setLastName(sc.next());
+                        break;
+                    case 3:
+                        System.out.println("Enter Address: ");
+                        personDetails.get(i).setAddress(sc.next());
+                        break;
+                    case 4:
+                        System.out.println("Enter City: ");
+                        personDetails.get(i).setCity(sc.next());
+                        break;
+                    case 5:
+                        System.out.println("Enter State: ");
+                        personDetails.get(i).setState(sc.next());
+                        break;
+                    case 6:
+                        System.out.println("Enter Zip: ");
+                        personDetails.get(i).setZip(sc.nextInt());
+                        break;
+                    case 7:
+                        System.out.println("Enter Mobile number: ");
+                        personDetails.get(i).setPhoneNumber(sc.nextLong());
+                        break;
+                    case 8:
+                        System.out.println("Enter new E-mail: ");
+                        personDetails.get(i).setEmailId(sc.next());
+                        break;
+                }
+                System.out.println("Edited list is: ");
+                System.out.println(personDetails);
+            } else
+                System.out.println("Enter a valid First name");
+        }
+    }
+
+    /*
     This method is used to display the added information
      */
     public void display() {
