702928ab074b48ab3af2d0b937b69d120f3b618b
Aniket1675 <Aniket@05>
2022-04-06 22:53:21 +0530

    ADDED: The welcome message.

---
+++ src/com/bridgelabz/addressbook/AddressBookSystemMain.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.addressbook;
+
+public class AddressBookSystemMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to address book system using stream");
+    }
+}
