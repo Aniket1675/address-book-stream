33527dfd2efde4398370e0b397a8be8ca4a4cf0d
Aniket1675 <Aniket@05>
2022-04-06 23:05:49 +0530

    ADDED: feature to add new contact in address book.

--- src/com/bridgelabz/addressbook/AddressBookSystemMain.java
+++ src/com/bridgelabz/addressbook/AddressBookSystemMain.java
@@ -5,6 +5,10 @@ public class AddressBookSystemMain {
     public static void main(String[] args) {
         System.out.println("Welcome to address book system using stream");
 
+        AddressBookSystemDetails details = new AddressBookSystemDetails();
+
+        details.addDetails();
+        details.display();
 
     }
 }
